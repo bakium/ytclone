e5336cdc72b936eaf7681a75e288442f600edc79
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 13:32:21 +0100

    Add lucide-react Menu icon

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,14 @@
+import { Menu } from 'lucide-react'
+
 const PageHeader = () => {
     return (
         <header className="flex space-between gap-10 lg:gap-20
           >:border-2 >:border-sky-500
         ">
             <section className="flex flex-shrink-0 gap-2">
-                <button>Menu</button>
+                <button>
+                    <Menu />
+                </button>
                 <a href="/">
                     <img src="../../public/vite.svg" className="h-6" />
                 </a>
