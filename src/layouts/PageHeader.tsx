d7cefe120e81f4b777c8db8ed791d0c01dce05a1
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 11:10:22 +0100

    Add PageHeader sections with flex attributes

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,5 +1,11 @@
 const PageHeader = () => {
-    return <header>PageHeader component</header>
+    return (
+        <header className="flex space-between gap-10 lg:gap-20">
+            <section>Left section</section>
+            <section>Middle section</section>
+            <section>Right section</section>
+        </header>
+    )
 }
 
 export { PageHeader }
