7bad1266a9c8e28205b1f99f24e4b2225c6f5cd7
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 11:33:55 +0100

    Add border to children with [$>*]

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,6 +1,8 @@
 const PageHeader = () => {
     return (
-        <header className="flex space-between gap-10 lg:gap-20">
+        <header className="flex space-between gap-10 lg:gap-20
+          [&>*]:border-2 [&>*]:border-sky-500
+        ">
             <section>Left section</section>
             <section>Middle section</section>
             <section>Right section</section>
