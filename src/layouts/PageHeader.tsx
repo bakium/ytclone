1fed790175e921341bbbaae38c8676f70279f483
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 15:10:32 +0100

    Add Button className overide support

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -7,7 +7,7 @@ const PageHeader = () => {
           >:border-2 >:border-sky-500
         ">
             <section className="flex flex-shrink-0 gap-2">
-                <Button size="icon">
+                <Button size="icon" className=''>
                     <Menu />
                 </Button>
                 <a href="/">
