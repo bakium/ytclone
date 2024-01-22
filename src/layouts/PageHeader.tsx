a96d74ce65d56ab90c3159c31e2ab99dd795f9d0
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 13:27:46 +0100

    Add PageHeader left section skeleton

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -3,7 +3,12 @@ const PageHeader = () => {
         <header className="flex space-between gap-10 lg:gap-20
           >:border-2 >:border-sky-500
         ">
-            <section>Left section</section>
+            <section className="flex flex-shrink-0 gap-2">
+                <button>Menu</button>
+                <a href="/">
+                    <img src="../../public/vite.svg" className="h-6" />
+                </a>
+            </section>
             <section>Middle section</section>
             <section>Right section</section>
         </header>
