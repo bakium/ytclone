23c8af4168094d718d00fbcdbead1af0297482e2
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-22 13:02:08 +0100

    Create > shorcut tailwind shortcut with addVariant()
    https://tailwindcss.com/docs/plugins#adding-variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [plugin(({ addVariant }) => {
+    addVariant('>', '&>*')
+  })],
 }
