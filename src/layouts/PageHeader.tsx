5c714b093447c2844331b29287de6de8e18a3958
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-24 17:18:12 +0100

    Replace middle section with a form

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,4 +1,4 @@
-import { Bell, Menu, User, Video } from 'lucide-react'
+import { Bell, Menu, Mic, Search, User, Video } from 'lucide-react'
 import { Button } from '../components/Button'
 
 const PageHeader = () => {
@@ -14,7 +14,9 @@ const PageHeader = () => {
                     <img src="/vite.svg" className="h-6" />
                 </a>
             </section>
-            <section>Middle section</section>
+            <form>
+                Form
+            </form>
             <section className="flex flex-row md:gap-2 flex-gap-4 flex-shrink-0">
                 <Button size="icon" variant="ghost"><Video /></Button>
                 <Button size="icon" variant="ghost"><Bell /></Button>
