75ef4493597f976c99064c53eacccf777c866016
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-25 15:41:41 +0100

    Fix max-height 100vh typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { PageHeader } from "./layouts/PageHeader"
 
 function App() {
   return (
-    <div className="max-h-size flex flex-col">
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <aside>Sidebar</aside>
+      <div className="grid grid-cols-[auto,1fr] overflow-auto">
+        <aside>Sidebar</aside>
+        <main className="min-h-[500px] border-2 border-secondary-dark bg-secondary">
+          Futur main content
+        </main>
+      </div>
     </div>
   )
 }
