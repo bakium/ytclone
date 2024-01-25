aa57a0e0f4ca86194dede2a4fd4d1c23583e77b9
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-25 16:24:40 +0100

    Add category and main video main container
    + To make the video goes under the categories
       parent: overflow-x-hidden
       child: sticky top-0 bg-white z-10
          here the bg white is important to avoid text overlap
    
    + To make the main video take all remaining space
       grid grid-cols-[auto,1fr]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { Button } from "./components/Button"
 import { PageHeader } from "./layouts/PageHeader"
 
 function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] overflow-auto">
-        <aside>Sidebar</aside>
-        <main className="min-h-[500px] border-2 border-secondary-dark bg-secondary">
-          Futur main content
-        </main>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <aside>Side</aside>
+        <section className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 px-5 pb-4">
+            <ul className="flex gap-2">
+              <li>
+                <Button>All</Button>
+              </li>
+              <li>
+                <Button>JavaScript</Button>
+              </li>
+            </ul>
+          </div>
+          <main className="min-h-[400px] bg-secondary">
+            <p>Video 1</p>
+            <p>Video 2</p>
+          </main>
+        </section>
       </div>
-    </div>
+    </div >
   )
 }
 
