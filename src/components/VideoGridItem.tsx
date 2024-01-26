94d1aedbb068b2dec6615303cb02b32557f76436
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-26 18:04:58 +0100

    Add links on elements

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -26,21 +26,30 @@ const VideoGridItem = ({
     console.log(title, id, channel, views, postedAt, duration, thumbnailUrl, videoUrl)
     return (
         <article className="flex flex-col gap-2 mb-4">
-            <div className="aspect-video">
+            <a href={`/watch?v=${id}`} className="aspect-video">
                 <img
                     src={thumbnailUrl}
                     className="block h-full w-full object-cover rounded-xl"
                 />
-            </div>
+            </a>
             <section className="flex gap-2 flex-shrink-0">
-                <img src={channel.profileUrl}
-                    alt={channel.name}
-                    className="w-10 h-10 rounded-full"
-                />
+                <a href={`@${channel.id}`}>
+                    <img src={channel.profileUrl}
+                        alt={channel.name}
+                        className="w-10 h-10 rounded-full"
+                    />
+                </a>
                 <section className="flex flex-col gap-1">
-                    <h3 className="font-bold">{title}</h3>
-                    <span>{channel.name}</span>
-                    <span className="text-secondary-text text-sm">
+                    <a href={`/watch?v=${id}`}>
+                        <h3 className="font-bold">{title}</h3>
+                    </a>
+                    <a
+                        href={`/@${channel.id}`}
+                        className="text-secondary-text text-sm"
+                    >
+                        {channel.name}</a>
+                    <span
+                        className="text-secondary-text text-sm">
                         {`${views} Views • ${postedAt.toString().length - 60} day ago`}
                     </span>
                 </section>
