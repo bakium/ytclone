9442051c4a5a1e6af4bfaf5256513a8fa78b8bd5
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-28 02:16:36 +0100

    Add dark button variant and apply it

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -6,10 +6,14 @@ type CategoryPillsProps = {
 
 export const CategoryPills = ({ categories }: CategoryPillsProps) => {
     return (
-        <ul className="flex gap-2">
+        <ul className="flex gap-3 w-[max-content]">
             {categories.map(category => (
-                <li key={category}>
-                    <Button>
+                <li key={category}
+                >
+                    <Button
+                        variant="dark"
+                        className="whitespace-nowrap text-sm py-1 px-3 rounded-lg"
+                    >
                         {category}
                     </Button>
                 </li>))}
